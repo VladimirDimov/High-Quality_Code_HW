9da29e1f8f52b7616155d6a05dde0d21e3218a1c
VladimirDimov <vdimov1124@gmail.com>
2015-07-05 00:38:25 +0300

    04. VariablesDataExpressionsAndConstants

---
+++ 04. Variables, Data, Expressions and Constants/VariablesDataExpressionsAndConstants/01. ClassSize/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class Size
+{
+    private double width;
+    private double height;
+
+    public Size(double w, double h)
+    {
+        this.Width = w;
+        this.Height = h;
+    }
+
+    public double Width
+    {
+        get
+        {
+            return this.width;
+        }
+
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Width must be greater than zero.");
+            }
+
+            this.width = value;
+        }
+    }
+
+    public double Height
+    {
+        get
+        {
+            return this.height;
+        }
+
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Height must be greater than zero.");
+            }
+
+            this.height = value;
+        }
+    }
+
+    public static Size GetRotatedSize(Size s, double angleOfTheFigureThatWillBeRotaed)
+    {
+        double sinOfAngleOfRotation = Math.Abs(Math.Sin(angleOfTheFigureThatWillBeRotaed));
+        double cosOfAngleOfRotation = Math.Abs(Math.Cos(angleOfTheFigureThatWillBeRotaed));
+
+        double widthProjection = (cosOfAngleOfRotation * s.width) + (sinOfAngleOfRotation * s.height);
+        double heightProjection = (sinOfAngleOfRotation * s.width) + (cosOfAngleOfRotation * s.height);
+
+        return new Size(widthProjection, heightProjection);
+    }
+}
---
+++ 04. Variables, Data, Expressions and Constants/VariablesDataExpressionsAndConstants/02.PrintStatistics/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _02.PrintStatistics
+{
+    using System;
+
+    public class Program
+    {
+        private static void Main()
+        {
+            PrintStatistics(new double[] { 1, 2, 3, 4, 5, 6 }, 3);
+        }
+
+        private static void PrintStatistics(double[] arr, int numberOfElements)
+        {
+            double max = double.MinValue;
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            PrintMax(max);
+
+            double min = double.MaxValue;
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            PrintMin(min);
+
+            double sum = 0;
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                sum += arr[i];
+            }
+
+            double average = sum / numberOfElements;
+
+            PrintAvg(average);
+        }
+
+        private static void PrintAvg(double p)
+        {
+            Console.WriteLine("Average value = " + p);
+        }
+
+        private static void PrintMin(double min)
+        {
+            Console.WriteLine("Minimal value = " + min);
+        }
+
+        private static void PrintMax(double max)
+        {
+            Console.WriteLine("Maximal value = " + max);
+        }
+    }
+}
