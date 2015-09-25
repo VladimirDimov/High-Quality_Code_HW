7ca3f5cb2d3b99b1d4e0702ecee80e92c4aacc55
VladimirDimov <vdimov1124@gmail.com>
2015-09-25 15:57:56 +0300

    Observer pattern added

---
+++ 16. BehavioralPattern/Observer/IInvestor.cs
@@ -0,0 +1,17 @@
+﻿using DoFactory.GangOfFour.Observer.RealWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    /// <summary>
+    /// The 'Observer' interface
+    /// </summary>
+    interface IInvestor
+    {
+        void Update(Stock stock);
+    }  
+}
