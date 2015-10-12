21a49a6ca1f2ca1c41fcc1df3e5469fa16dcb0b5
VladimirDimov <vdimov1124@gmail.com>
2015-10-12 11:53:28 +0300

    Mocking added

--- 17. Mocking/Cars.Tests.JustMock/CarsControllerTests.cs
+++ 17. Mocking/Cars.Tests.JustMock/CarsControllerTests.cs
@@ -9,7 +9,7 @@
     using Cars.Tests.JustMock.Mocks;
     using Cars.Controllers;
     using Cars.Models;
-    
+
     [TestClass]
     public class CarsControllerTests
     {
@@ -18,7 +18,6 @@
 
         public CarsControllerTests()
             : this(new JustMockCarsRepository())
-            // : this(new MoqCarsRepository())
         {
         }
 
@@ -95,6 +94,126 @@
             Assert.AreEqual("Audi", model.Make);
             Assert.AreEqual("A5", model.Model);
             Assert.AreEqual(2005, model.Year);
+        }
+
+        [TestMethod]
+        public void SearchShouldWorkPropperlyWithMake()
+        {
+            var cars = (IList<Car>)this.GetModel(() => this.controller.Search("Audi"));
+
+            Assert.IsNotNull(cars);
+            Assert.AreEqual(2, cars.Count);
+
+            foreach (var car in cars)
+            {
+                Assert.AreEqual("Audi", car.Make);
+            }
+        }
+
+        [TestMethod]
+        public void SearchShouldWorkPropperlyWithModel()
+        {
+            var cars = (IList<Car>)this.GetModel(() => this.controller.Search("Astra"));
+
+            Assert.IsNotNull(cars);
+            Assert.AreEqual(1, cars.Count);
+            foreach (var car in cars)
+            {
+                Assert.AreEqual("Opel", car.Make);
+            }
+        }
+
+        [TestMethod]
+        public void SearchShouldReturnWholeListWhenEmptyInput()
+        {
+            var cars = (IList<Car>)this.GetModel(() => this.controller.Search(""));
+
+            Assert.IsNotNull(cars);
+            Assert.AreEqual(4, cars.Count);
+        }
+
+        [TestMethod]
+        public void SearchShouldReturnWholeListWhenNullInput()
+        {
+            var cars = (IList<Car>)this.GetModel(() => this.controller.Search(null));
+
+            Assert.IsNotNull(cars);
+            Assert.AreEqual(4, cars.Count);
+        }
+
+        [TestMethod]
+        public void SearchShouldReturnNothingWhenBadInput()
+        {
+            var cars = (IList<Car>)this.GetModel(() => this.controller.Search("pesho"));
+
+            Assert.IsNotNull(cars);
+            Assert.AreEqual(0, cars.Count);
+        }
+
+        [TestMethod]
+        public void DetailShouldWorkCorrectlyWhenProvidedValidId()
+        {
+            var detailsForCar = (Car)this.GetModel(() => this.controller.Details(1));
+
+            Assert.AreEqual(1, detailsForCar.Id);
+            Assert.AreEqual("Audi", detailsForCar.Make);
+            Assert.AreEqual(2005, detailsForCar.Year);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void DetailShouldThrowWhenArgumentNullExceptionProvidedInvalidInput()
+        {
+            var detailsForCar = (Car)this.GetModel(() => this.controller.Details(0));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SortByMakeShouldThrowArgumentExceptionWhenEmptyInput()
+        {
+            var cars = (IList<Car>)this.GetModel(() => this.controller.Sort(" "));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SortByMakeShouldThrowArgumentExceptionWhenNullInput()
+        {
+            var cars = (IList<Car>)this.GetModel(() => this.controller.Sort(null));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SortByMakeShouldThrowArgumentExceptionWhenWrongInput()
+        {
+            var cars = (IList<Car>)this.GetModel(() => this.controller.Sort("model"));
+        }
+
+        [TestMethod]
+        public void SortByMakeShouldWorkCorrectly()
+        {
+            var cars = (IList<Car>)this.GetModel(() => this.controller.Sort("make"));
+
+            Assert.IsNotNull(cars);
+            Assert.AreEqual(4, cars.Count);
+
+            Assert.AreEqual("Audi", cars[0].Make);
+            Assert.AreEqual("Audi", cars[1].Make);
+            Assert.AreEqual("BMW", cars[2].Make);
+            Assert.AreEqual("Opel", cars[3].Make);
+        }
+
+        [TestMethod]
+        public void SortByYearShouldWorkCorrectly()
+        {
+            var cars = (IList<Car>)this.GetModel(() => this.controller.Sort("year"));
+
+            Assert.IsNotNull(cars);
+            Assert.AreEqual(4, cars.Count);
+
+            Assert.AreEqual(2005, cars[0].Year);
+            Assert.AreEqual(2007, cars[1].Year);
+            Assert.AreEqual(2008, cars[2].Year);
+            Assert.AreEqual(2010, cars[3].Year);
         }
 
         private object GetModel(Func<IView> funcView)
--- 17. Mocking/Cars.Tests.JustMock/Mocks/JustMockCarsRepository.cs
+++ 17. Mocking/Cars.Tests.JustMock/Mocks/JustMockCarsRepository.cs
@@ -1,4 +1,8 @@
-﻿namespace Cars.Tests.JustMock.Mocks
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Cars.Tests.JustMock.Mocks
 {
     using System.Linq;
 
@@ -14,8 +18,21 @@
             this.CarsData = Mock.Create<ICarsRepository>();
             Mock.Arrange(() => this.CarsData.Add(Arg.IsAny<Car>())).DoNothing();
             Mock.Arrange(() => this.CarsData.All()).Returns(this.FakeCarCollection);
-            Mock.Arrange(() => this.CarsData.Search(Arg.AnyString)).Returns(this.FakeCarCollection.Where(c => c.Make == "BMW").ToList());
-            Mock.Arrange(() => this.CarsData.GetById(Arg.AnyInt)).Returns(this.FakeCarCollection.First());
+            Mock.Arrange(() => this.CarsData.Search(Arg.AnyString))
+                .Returns((string search) =>
+                    this.FakeCarCollection
+                            .Where(c => c.Make.ToLower() == search.ToLower() ||
+                                    c.Model.ToLower() == search.ToLower())
+                            .ToList());
+            Mock.Arrange(() => this.CarsData.Search(Arg.NullOrEmpty))
+                .Returns(this.FakeCarCollection.ToList());
+
+            Mock.Arrange(() => this.CarsData.GetById(Arg.AnyInt)).Returns((int id) => this.FakeCarCollection.First());
+            Mock.Arrange(() => this.CarsData.GetById(Arg.IsInRange(0, this.FakeCarCollection.Count, RangeKind.Exclusive))).Returns((int id) => this.FakeCarCollection.First(car => car.Id == id));
+            Mock.Arrange(() => this.CarsData.GetById(Arg.IsInRange(Int32.MinValue, 0, RangeKind.Inclusive))).DoNothing();
+
+            Mock.Arrange(() => this.CarsData.SortedByMake()).Returns(this.FakeCarCollection.OrderBy(c => c.Make).ToList());
+            Mock.Arrange(() => this.CarsData.SortedByYear()).Returns(this.FakeCarCollection.OrderBy(c => c.Year).ToList());
         }
     }
 }
